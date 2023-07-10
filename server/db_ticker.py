338befe7c182ae74078f07c6b694e92b44b74c62
bohndiggin <bohndiggin@gmail.com>
2023-07-10 14:56:37 -0600

    minor adjustments, optimizations and notes

--- server/db_ticker.py
+++ server/db_ticker.py
@@ -1,10 +1,10 @@
 # THIS FILE IS TO TRACK TIME AND REGULARLY CHECK DB
-import requests, schedule, sys # LOOK UP SCHEDULE DOCUMENATION FOR "TODAY"
+# Every hour, gather the next 90 minutes worth of reminders and sort them
+import schedule
 from dotenv import load_dotenv
 from utils import *
 import psycopg2, psycopg2.extras
 from multiprocessing import Process
-# import subprocess as sp
 
 load_dotenv()
 
@@ -15,21 +15,16 @@ server_url = os.getenv('SERVER_URL')
 next_90 = []
 successfully_reminded = []
 
-# Every hour, gather the next 90 minutes worth of reminders and sort them
-# AT midnight, change the day of the week
-# "EVERY OTHER TUESDAY, WEDNESDAY at 10pm"
-# ADD time as a parameter so I can run queries immediately
-def reminder_func_paralell(obj, successfully_reminded):
-    reminder_worked = obj.remind()
+def reminder_func_paralell(obj, successfully_reminded): # This allows the main remind_it() to spawn more reminders than can be processed in a minute, that way we don't run out of time to remind.
+    reminder_worked = obj.remind() # if the reminder suceeded, it returns true
     if reminder_worked == True:
         successfully_reminded.append(obj.id)
         print(f'reminded! ID: {obj.id} __ Offset: {obj.real_offset}')
     elif reminder_worked == False:
-        # print('skipped: incorrect time')
         pass
     return 'complete'
 
-def remind_query():
+def remind_query(): # This will query the database and create objects for each reminder. Should fire every 60 min. It then gathers the next 90 min (on a -2 min offset) of reminders.
     global next_90, successfully_reminded
     try:
         conn = psycopg2.connect(db_url)
@@ -68,7 +63,7 @@ def remind_query():
         print('remind_query returned no reminders')
     return next_90
 
-def remind_it():
+def remind_it(): # makes a list of reminder processes and starts them all.
     global next_90, successfully_reminded
     try:
         for i in next_90:
@@ -89,21 +84,12 @@ def remind_it():
 schedule.every().hour.do(remind_query)
 schedule.every().minute.do(remind_it)
 
-def main(): # MAKE IT SET TO BUSY WHEN IT'S BUSY _ Make 2 QUEUES IN-flight and staged
+def main(): 
     while True:
         schedule.run_pending()
         time.sleep(1)
-        # remind_it()
-
-def test():
-    test_time = datetime.datetime(2023, 7, 5, 5, 29, 0)
-    remind_query(now=test_time)
-    pass
 
 if __name__ == '__main__':
-    # seed_funciton()
-    # add_function()
     remind_query()
     remind_it()
-    # test()
     main()
