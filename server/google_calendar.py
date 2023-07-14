610f825499630a69f2fb127c06f5b7ad37ad0c59
bohndiggin <bohndiggin@gmail.com>
2023-07-14 15:19:42 -0600

    reminders should work now

--- server/google_calendar.py
+++ server/google_calendar.py
@@ -25,7 +25,7 @@ def calendar_add(title, description):
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('.\.credentials\credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('.\credentials\credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
